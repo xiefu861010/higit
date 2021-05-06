da1fd2a335a593b418a60c8dd2455c09b4346ded
xiefu861010 <xiefu861010@163.com>
2021-05-06 12:07:26 +0800

    创建版本

---
+++ higit/src/main/java/com/jiotian/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.jiotian.git;
+
+public class HelloGit {
+
+        public static void main(String[] args){
+            System.out.println("hello,git");
+            System.out.println("更新");
+            System.out.println("更新1");
+            System.out.println("更新111");
+        }
+
+
+}
