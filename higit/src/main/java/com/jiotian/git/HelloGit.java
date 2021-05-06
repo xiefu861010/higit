9b0c00c98218dad0436d0177ee4cc90b42cb5cce
xiefu861010 <xiefu861010@163.com>
2021-05-06 12:32:47 +0800

    主干添加

--- higit/src/main/java/com/jiotian/git/HelloGit.java
+++ higit/src/main/java/com/jiotian/git/HelloGit.java
@@ -8,7 +8,8 @@ public class HelloGit {
             System.out.println("更新1");
             System.out.println("更新111");
             System.out.println("创建分支");
-            System.out.println("主干添加");
+            System.out.println("主干添加完成");
+
         }
 
 
